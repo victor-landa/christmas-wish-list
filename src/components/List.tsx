80af2cd3851b2dc54af26da98534b0c3be8be36b
Victor Landa <victorlanda.dev@gmail.com>
2021-12-21 10:35:31 -0600

    :sparkles: day 13: add the edit mode

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 import { Modal } from './Modal';
 import { Overlay } from './Overlay';
 import '../assets/styles/components/List.css';
@@ -15,10 +15,18 @@ export const List = () => {
   const [list, setList] = useState<Array<WishListItem>>(localStorageList ? JSON.parse(localStorageList) : []);
   const [modalState, setModalState] = useState(false);
   const [overlay, setOverlay] = useState(false);
+  const [formValues, setFormValues] = useState<WishListItem>({
+    title: '',
+    recipient: '',
+    quantity: 1,
+    source: ''
+  });
+  const [editMode, setEditMode] = useState(false);
+  const [editedItem, setEditedItem] = useState<Number>();
 
   const handleForm = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    const inputTitle = document.querySelector<HTMLInputElement>('input[name="gift"]');
+    const inputTitle = document.querySelector<HTMLInputElement>('input[name="title"]');
     const inputRecipient = document.querySelector<HTMLInputElement>('input[name="recipient"]');
     const inputQuantity = document.querySelector<HTMLInputElement>('input[name="quantity"]');
     const inputSource = document.querySelector<HTMLInputElement>('input[name="source"]');
@@ -30,25 +38,36 @@ export const List = () => {
             repeatedItem = true;
           }
         })
-        if(!repeatedItem) {
-          setList([
-            ...list,
-            {
-              title: inputTitle.value.trim(),
-              recipient: inputRecipient.value.trim(),
-              quantity: Number(inputQuantity.value),
-              source: inputSource.value.trim(),
-            }]);
-          localStorage.setItem('wish-list', JSON.stringify([
-            ...list,
-            {
-              title: inputTitle.value.trim(),
-              recipient: inputRecipient.value.trim(),
-              quantity: Number(inputQuantity.value),
-              source: inputSource.value.trim()
-            }
-          ]));
-          handleToggleModal();
+        if(editMode) {
+          if(typeof editedItem === 'number') {
+            list[editedItem].title = inputTitle.value.trim();
+            list[editedItem].recipient = inputRecipient.value.trim();
+            list[editedItem].quantity =Number(inputQuantity.value);
+            list[editedItem].source = inputSource.value.trim();
+            localStorage.setItem('wish-list', JSON.stringify([...list]));
+            handleToggleModal();
+          }
+        } else {
+          if(!repeatedItem) {
+            setList([
+              ...list,
+              {
+                title: inputTitle.value.trim(),
+                recipient: inputRecipient.value.trim(),
+                quantity: Number(inputQuantity.value),
+                source: inputSource.value.trim(),
+              }]);
+            localStorage.setItem('wish-list', JSON.stringify([
+              ...list,
+              {
+                title: inputTitle.value.trim(),
+                recipient: inputRecipient.value.trim(),
+                quantity: Number(inputQuantity.value),
+                source: inputSource.value.trim()
+              }
+            ]));
+            handleToggleModal();
+          }
         }
         inputTitle.value = '';
         inputRecipient.value = '';
@@ -66,19 +85,45 @@ export const List = () => {
     localStorage.setItem('wish-list', JSON.stringify(list.filter(item => item.title !== title)));
   }
 
+  const handleEditItem = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLSpanElement;
+    const title = target.dataset.itemTitle;
+    const selectedItem = list.find(item => item.title === title);
+    const selectedItemIndex = list.findIndex(item => item === selectedItem);
+    setEditMode(true);
+    setEditedItem(selectedItemIndex);
+    if(selectedItem) {
+      setFormValues({
+        title: selectedItem.title,
+        recipient: selectedItem.recipient,
+        quantity: selectedItem.quantity,
+        source: selectedItem.source
+      });
+      handleToggleModal();
+    }
+  }
+
   const handleClearList = () => {
     setList([]);
     localStorage.setItem('wish-list', JSON.stringify([]));
   }
 
+  const handleInputChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  }
+
   const form = () => {
     return (
       <form className="form" onSubmit={handleForm} autoComplete="off">
-        <input className="form__input" type="text" name="gift" placeholder="New gift" required />
-        <input className="form__input form__input--below" type="text" name="recipient" placeholder="This gift is to..." required />
-        <input className="form__input form__input--below" type="number" min="1" name="quantity" placeholder="Quantity" required />
-        <input className="form__input form__input--below" type="url" name="source" placeholder="Image link" pattern="https://.*" required />
-        <button className="form__button" type="submit">Add</button>
+        <input className="form__input" type="text" name="title" value={formValues.title} onChange={handleInputChange} placeholder="New gift" required />
+        <input className="form__input form__input--below" type="text" name="recipient" value={formValues.recipient} onChange={handleInputChange} placeholder="This gift is to..." required />
+        <input className="form__input form__input--below" type="number" min="1" name="quantity" value={formValues.quantity} onChange={handleInputChange} placeholder="Quantity" required />
+        <input className="form__input form__input--below" type="url" name="source" value={formValues.source} onChange={handleInputChange} placeholder="Image link" pattern="https://.*" required />
+        <button className="form__button" type="submit">{editMode ? 'Edit' :  'Add'}</button>
         <button className="form__button form__button--close" type="button" onClick={handleToggleModal}>Close</button>
       </form>
     )
@@ -87,6 +132,17 @@ export const List = () => {
   const handleToggleModal = () => {
     setOverlay(!overlay);
     setModalState(!modalState);
+    setTimeout(() => {
+      if(overlay && modalState) {
+        setEditMode(false);
+        setFormValues({
+          title: '',
+          recipient: '',
+          quantity: 1,
+          source: ''
+        });
+      }
+    }, 250);
   }
 
   return (
@@ -104,12 +160,20 @@ export const List = () => {
                     <span className="wish-list-card__item-recipient">{item.recipient}</span>
                   </div>
                 </div>
-                <span
-                  className="wish-list-card__item-delete"
-                  data-item-title={item.title}
-                  onClick={handleDeleteItem}>
-                  x
-                </span>
+                <div className="wish-list-card__actions">
+                  <span
+                    className="wish-list-card__action wish-list-card__action--edit"
+                    data-item-title={item.title}
+                    onClick={handleEditItem}>
+                    e
+                  </span>
+                  <span
+                    className="wish-list-card__action wish-list-card__action--delete"
+                    data-item-title={item.title}
+                    onClick={handleDeleteItem}>
+                    x
+                  </span>
+                </div>
               </li>
             )}
           </ul>
